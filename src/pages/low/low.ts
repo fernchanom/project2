99c3d279b29251f6f15cd218bed428441951b03f
supapan <supapan@realsmart.in.th>
2019-04-09 16:10:43 +0700

    risk type

--- src/pages/low/low.ts
+++ src/pages/low/low.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import firebase from 'firebase';
+import { DetailpatientPage } from '../detailpatient/detailpatient';
 
 /**
  * Generated class for the LowPage page.
@@ -14,8 +16,73 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'low.html',
 })
 export class LowPage {
+    patient = [];
+    key:string = null;
+    firstName:string = null;
+    lastName:string = null;
+    sex:string = null;
+    dateOfBirth:string = null;
+    age:string = null;
+    bloodType:string = null;
+    medicalProblems:string = null;
+    riskType:string = null;
+    address:string = null;
+    tel:string = null;
+    patient_id:string = null;
+    identification_number:string = null;
+    captureDataUrl: string = null;
+    data = {
+      firstName : null,
+      lastName : null,
+      sex : null,
+      dateOfBirth : null,
+      age : null,
+      bloodType : null,
+      medicalProblems : null,
+      riskType : null,
+      address : null,
+      tel : null,
+      patient_id : null,
+      identification_number : null,
+      urlImg : null
+    };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  {
+		const requestRef = firebase.database().ref('/Patient/');
+		requestRef.orderByChild('riskType')
+		          .equalTo('ต่ำ')
+		          .once('value')
+		          .then(snapshot => snapshot.val())
+		          .then((data) => {
+		          	this.patient = Object.keys(data).map(function(index){
+						    	return data[index];
+								});
+		          });
+  }
+
+  //กําหนดค่าให้กับ input และเก็บ key 
+  select(item) {
+    //console.log(item);
+    this.firstName = item.firstName;
+    this.lastName = item.lastName;
+    this.sex = item.sex;
+    this.dateOfBirth = item.dateOfBirth;
+    this.age = item.age;
+    this.bloodType = item.bloodType;
+    this.medicalProblems = item.medicalProblems;
+    this.riskType = item.riskType;
+    this.address = item.address;
+    this.tel = item.tel;
+    this.patient_id = item.patient_id;
+    this.identification_number = item.identification_number;
+    this.key = item.key;
+  }
+
+  //แสดงข้อมูลคนไข้
+  goToDetailpatient(key,patient) {
+    // console.log("patient: ",patient);
+    this.navCtrl.push(DetailpatientPage,{key: key, patient: patient}); //ไปหน้า Detailpatient พร้อมส่งค่าตัวแปร key & patient
   }
 
   ionViewDidLoad() {
